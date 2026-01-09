52ccbc720e4efbc4caf99ff42acfb19ffb7d738b
Ankit2515ry <ankit2515ry@gmail.com>
2026-01-09 13:00:46 +0000

    Code challenge submission for question: 2695 Array Wrapper

---
+++ 2695 Array Wrapper/2695array-wrapper.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums=nums;
+    //console.log(this.nums);
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    //console.log(this.nums);
+    let sum=0;
+    for(let i=0;i<this.nums.length;i++){
+        sum+=this.nums[i];
+    }
+    return sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    //console.log(this.nums);
+    let str = "[";
+    for (let i = 0; i < this.nums.length; i++) {
+        str += this.nums[i];
+        if (i < this.nums.length - 1) {
+            str += ",";
+        }
+    }
+    str += "]";
+    return str;
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
