d73bf5ef9ebab36c38e715c8b90b31bcb37fb97e
ANKIT KUMAR <ankit2515ry@gmail.com>
2026-08-22 23:55:55 +0530

    Time: 3 ms (90.26%) | Memory: 9.1 MB (23.31%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
+    vector<int>dp;
     int solve(int n,int mask){
         int c=__builtin_popcount(mask);
         if(c==n)return 1;
+        if(dp[mask]!=-1)return dp[mask];
         int res=0;
         for(int i=1;i<=n;i++){
             if(!((mask>>i)&1)){
@@ -12,9 +14,11 @@ public:
                 }
             }
         }
-        return res;
+        return dp[mask]=res;
     }
     int countArrangement(int n) {
+        int sz=1<<(n+1);
+        dp.assign(sz,-1);
         return solve(n,0);
     }
 };
