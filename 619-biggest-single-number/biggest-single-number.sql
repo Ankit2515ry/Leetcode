a7e78e660bb723f5a0504b869e33e75b52fb37a1
ANKIT KUMAR <ankit2515ry@gmail.com>
2026-08-30 11:33:06 +0530

    Time: 438 ms (75.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+-- select max(num) as num from (
+--     select num 
+--     from Mynumbers 
+--     group by num
+--     having count(num)=1   
+-- ) as un;
+select (
+    select num 
+    from MyNumbers
+    group by num
+    having count(num)=1
+    order by num desc
+    limit 1
+) as num;
